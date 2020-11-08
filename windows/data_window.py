c539248f96af60dca6500a255624c82ed2bae891
Archais <aipt321@gmail.com>
2020-11-08 19:52:28 +0200

    Added money totals tab that displays: current balance, debtors, and creditors.

--- windows/data_window.py
+++ windows/data_window.py
@@ -2,7 +2,7 @@
 import sqlite3 as sql
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QHeaderView, QLabel, QTableWidget,
+from PyQt5.QtWidgets import (QHBoxLayout, QHeaderView, QLabel, QTableWidget,
                              QTableWidgetItem, QTabWidget, QVBoxLayout,
                              QWidget)
 
@@ -31,14 +31,14 @@ class DataWindow(QWidget):
         self.tabs = QTabWidget()
         self.tabs.setTabPosition(QTabWidget.North)
 
+        # Table for transaction history
         self.content_table = QTableWidget()
         self.content_query = 'select Date, Activity, "Transaction Type", "Other Party", '\
             'round(Value, 2) Value from finance order by ID desc'
-        if self.populate_table(self.content_table, self.content_query):
-            num_rows = self.content_table.rowCount()
-            if num_rows > 0:
-                self.tabs.addTab(self.content_table, 'Contents')
-            else:
+        self.content_table_columns = [
+            'Date', 'Activity', 'Transaction Type', 'Other Party', 'Value']
+        if self.populate_table(self.content_table, self.content_table_columns, self.content_query):
+            if self.content_table.rowCount() < 1:
                 self.content_table = QLabel('No results to display')
                 self.content_table.setAlignment(Qt.AlignCenter)
                 font = self.content_table.font()
@@ -52,6 +52,56 @@ class DataWindow(QWidget):
             font.setPointSize(font.pointSize() * 5)
             self.content_table.setFont(font)
         self.tabs.addTab(self.content_table, 'Transaction History')
+
+        # Money totals tab
+        self.money_totals = QWidget()
+        self.totals_layout = QVBoxLayout()
+        self.totals_layout.setAlignment(Qt.AlignCenter)
+
+        # User balance
+        self.balance_row = QHBoxLayout()
+        self.balance_lbl = QLabel("Balance: ")
+        self.balance_row.addWidget(self.balance_lbl)
+        self.balance_query = r"select round(sum(case when `Transaction Type` like '%received%' or "\
+            r"`Transaction Type` like '%return%' or `Transaction Type` like '%borrow from%' then "\
+            r"Value else Value * -1 end), 2) balance from finance"
+        self.balance = self.run_query(self.balance_query)[0]['balance']
+        self.balance_amnt = QLabel(
+            '$' + str(self.balance if self.balance else 0))
+        self.balance_row.addWidget(self.balance_amnt)
+        self.balance_row.setAlignment(Qt.AlignLeft)
+        self.totals_layout.addLayout(self.balance_row)
+
+        # Debtors table
+        self.debtors = QTableWidget()
+        self.debtors_columns = ["Debtor", "Amount Owed"]
+        self.debtors_query = "select `Other Party` Debtor, sum(case when `Transaction Type` == "\
+            "'Loan' then Value else (case when `Transaction Type` == 'Loan Return' then Value * -1"\
+            " else 0 end) end) 'Amount Owed' from finance group by Debtor having `Amount Owed` != "\
+            "0 order by `Amount Owed` desc"
+        if self.populate_table(self.debtors, self.debtors_columns, self.debtors_query):
+            if self.debtors.rowCount() < 1:
+                self.debtors = QLabel("There are currently no debtors.")
+        else:
+            self.debtors = QLabel("Error displaying debtors.")
+        self.totals_layout.addWidget(self.debtors)
+
+        # Creditors table
+        self.creditors = QTableWidget()
+        self.creditors_columns = ["Creditor", "Amount Owed"]
+        self.creditors_query = "select `Other Party` Creditor, sum(case when `Transaction Type` =="\
+            " 'Borrow From' then Value else (case when `Transaction Type` == 'Payback' then Value"\
+            " * -1 else 0 end) end) 'Amount Owed' from finance group by Creditor having `Amount "\
+            "Owed` != 0 order by `Amount Owed` desc"
+        if self.populate_table(self.creditors, self.creditors_columns, self.creditors_query):
+            if self.creditors.rowCount() < 1:
+                self.creditors = QLabel("There are currently no creditors.")
+        else:
+            self.creditors = QLabel("Error displaying creditors.")
+        self.totals_layout.addWidget(self.creditors)
+
+        self.money_totals.setLayout(self.totals_layout)
+        self.tabs.addTab(self.money_totals, 'Totals')
 
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.tabs)
@@ -70,19 +120,18 @@ class DataWindow(QWidget):
             error_msg.exec_()
             return None
 
-    def populate_table(self, table: QTableWidget, query: str):
+    def populate_table(self, table: QTableWidget, columns: list, query: str):
         "Populate the given table"
         try:
             export = self.run_query(query)
             if export:
                 table.setRowCount(len(export))
-                table.setColumnCount(5)
+                table.setColumnCount(len(columns))
 
                 table.horizontalHeader().setStretchLastSection(True)
-                table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+                table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
 
-                columns = ['Date', 'Activity',
-                           'Transaction Type', 'Other Party', 'Value']
+                table.setSortingEnabled(True)
 
                 for i, title in enumerate(columns):
                     table.setHorizontalHeaderItem(i, QTableWidgetItem(title))
@@ -92,7 +141,9 @@ class DataWindow(QWidget):
                         item = str(export[row[0]][col])
                         if col == 'Value':
                             item = '$' + item
-                        table.setItem(row[0], column, QTableWidgetItem(item))
+                        item = QTableWidgetItem(item)
+                        item.setFlags(Qt.ItemIsEnabled)
+                        table.setItem(row[0], column, item)
             return True
         except Exception:
             return False
