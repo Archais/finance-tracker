7276317bd03fb58cfce473fd951212a64ba43dd1
Archais <aipt321@gmail.com>
2020-12-27 01:25:26 +0200

    Implemented Income & Expense graph in data window.

--- windows/data_window.py
+++ windows/data_window.py
@@ -1,7 +1,9 @@
 "Contains code related to the data window."
 import sqlite3 as sql
+import pyqtgraph as pg
 
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette
 from PyQt5.QtWidgets import (QHBoxLayout, QHeaderView, QLabel, QTableWidget,
                              QTableWidgetItem, QTabWidget, QVBoxLayout,
                              QWidget)
@@ -30,28 +32,6 @@ class DataWindow(QWidget):
 
         self.tabs = QTabWidget()
         self.tabs.setTabPosition(QTabWidget.North)
-
-        # Table for transaction history
-        self.content_table = QTableWidget()
-        self.content_query = 'select Date, Activity, "Transaction Type", "Other Party", '\
-            'round(Value, 2) Value from finance order by ID desc'
-        self.content_table_columns = [
-            'Date', 'Activity', 'Transaction Type', 'Other Party', 'Value']
-        if self.populate_table(self.content_table, self.content_table_columns, self.content_query):
-            if self.content_table.rowCount() < 1:
-                self.content_table = QLabel('No results to display')
-                self.content_table.setAlignment(Qt.AlignCenter)
-                font = self.content_table.font()
-                font.setPointSize(font.pointSize() * 5)
-                self.content_table.setFont(font)
-        else:
-            self.content_table = QLabel(
-                'Error occurred when trying to display results.')
-            self.content_table.setAlignment(Qt.AlignCenter)
-            font = self.content_table.font()
-            font.setPointSize(font.pointSize() * 5)
-            self.content_table.setFont(font)
-        self.tabs.addTab(self.content_table, 'Transaction History')
 
         # Money totals tab
         self.money_totals = QWidget()
@@ -103,6 +83,38 @@ class DataWindow(QWidget):
         self.money_totals.setLayout(self.totals_layout)
         self.tabs.addTab(self.money_totals, 'Totals')
 
+        # Table for transaction history
+        self.content_table = QTableWidget()
+        self.content_query = 'select Date, Activity, "Transaction Type", "Other Party", '\
+            'round(Value, 2) Value from finance order by ID desc'
+        self.content_table_columns = [
+            'Date', 'Activity', 'Transaction Type', 'Other Party', 'Value']
+        if self.populate_table(self.content_table, self.content_table_columns, self.content_query):
+            if self.content_table.rowCount() < 1:
+                self.content_table = QLabel('No results to display')
+                self.content_table.setAlignment(Qt.AlignCenter)
+                font = self.content_table.font()
+                font.setPointSize(font.pointSize() * 5)
+                self.content_table.setFont(font)
+        else:
+            self.content_table = QLabel(
+                'Error occurred when trying to display results.')
+            self.content_table.setAlignment(Qt.AlignCenter)
+            font = self.content_table.font()
+            font.setPointSize(font.pointSize() * 5)
+            self.content_table.setFont(font)
+        self.tabs.addTab(self.content_table, 'Transaction History')
+
+        # Balance graphs across the year
+        self.metrics = QWidget()
+        self.metrics_layout = QVBoxLayout(self.metrics)
+
+        self.income_and_expense = pg.PlotWidget()
+        self.draw_ie_chart()
+        self.metrics_layout.addWidget(self.income_and_expense)
+
+        self.tabs.addTab(self.metrics, "Graphs")
+
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
@@ -148,3 +160,53 @@ class DataWindow(QWidget):
             return True
         except Exception:
             return False
+
+    def draw_ie_chart(self):
+        "Draw chart for monthly income & expense."
+        income_query = r"select strftime('%m', Date) Month, sum(case when `Transaction "\
+            r"Type` in ('Payment Received', 'Loan Return', 'Borrow From') then Value else 0 end) "\
+            r"Income from finance where strftime('%Y', Date) = "\
+            r"strftime('%Y', 'now') group by strftime('%m', Date);"
+        expense_query = r"select strftime('%m', Date) Month, sum(case when `Transaction "\
+            r"Type` in ('Paid', 'Loan', 'Payback') then Value else 0 end) "\
+            r"Expense from finance where strftime('%Y', Date) = "\
+            r"strftime('%Y', 'now') group by strftime('%m', Date);"
+
+        try:
+            income_pen = pg.mkPen(color=(0, 255, 0))
+            expense_pen = pg.mkPen(color=(255, 0, 0))
+            self.income_and_expense.setXRange(1, 12)
+            self.income_and_expense.addLegend()
+            self.draw_graph(self.income_and_expense,
+                            income_query, income_pen, "Income")
+            self.draw_graph(self.income_and_expense,
+                            expense_query, expense_pen, "Expense")
+            self.income_and_expense.setTitle("Monthly Income & Expense")
+            self.income_and_expense.setLabel('left', "Money ($)")
+            self.income_and_expense.setLabel('bottom', "Month")
+            self.income_and_expense.setBackground(
+                self.palette().color(QPalette.Window))
+        except Exception:
+            self.income_and_expense = QLabel(
+                "Unable to draw Income & Expense graph.")
+            self.income_and_expense.setAlignment(Qt.AlignCenter)
+
+    def draw_graph(self, graph, query: str, pen=None, name: str = None):
+        "Draw graph based on the given query."
+        results = self.run_query(query, False)
+        index = list(range(1, 13))
+        values = [0 for i in range(1, 13)]
+        for record in results:
+            values[int(record[0])] = float(record[1])
+            #index.append(int(record[0]))
+            #values.append(float(record[1]))
+
+        for i in range(1, 13):
+            if i not in index:
+                index.append(i)
+                values.append(0)
+
+        if pen:
+            graph.plot(index, values, pen=pen, name=name)
+        else:
+            graph.plot(index, values, name=name)
